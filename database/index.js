4765bb34963d9ab6a5b95f7ef36e5753857ff189
Vinh <hoangqv92@gmail.com>
2019-10-11 11:40:35 -0700

    implemented seedDatabase

--- database/index.js
+++ database/index.js
@@ -1,37 +1,9 @@
-const mongoose = require ('mongoose');
-const faker = require ('faker');
+const mongoose = require('mongoose');
+const faker = require('faker');
 
-mongoose.connect('mongodb://localhost/gallery');
+mongoose.connect('mongodb://localhost/photos');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection Error: '));
-db.once('open', () => {
-    console.log('Connected to DB')
-});
+const db = mongoose.connection;
 
-var Schema = mongoose.Schema;
-
-var imgSchema = new Schema({
-    url: String,
-    listingId: Number,
-    pictureId: Number,
-    caption: String
-});
-
-var Gallery = mongoose.model('Galleries', imgSchema);
-
-var test = {
-    url: 'skdksdk'
-}
-const filter = {listingId: 0};
-const update = {caption: 'hi Vita'};
-Gallery.findOneAndUpdate(filter, update, {
-    new: true,
-    upsert: true
-}, (err, doc) => {
-    if (err) {
-        console.log(err);
-    }
-});
-
-console.log(faker.image.animals());
+db.on('err', console.error.bind('err'));
+db.once('open', () => {console.log('DB Connected')});
